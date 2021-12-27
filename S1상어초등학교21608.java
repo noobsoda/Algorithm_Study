51268ff6cc2363065267957d2c98317a0c82b0ca
noobsoda <20151565@vision.hoseo.edu>
2021-12-27 17:02:39 +0900

    2021_12_27 구현 study

--- S1상어초등학교21608.java
+++ S1상어초등학교21608.java
@@ -5,7 +5,7 @@ public class S1상어초등학교21608 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        
+        String nv[];
     }
 }
     
