3325bb1cf2bb54a80da6ada42151ea283f851770
noobsoda <20151565@vision.hoseo.edu>
2021-12-30 00:47:37 +0900

    2021

---
+++ G4게리맨더링2_17779.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class G4게리맨더링2_17779 {
+    static int N;
+    static int map[][];
+
+    public static void gerrymandering2(){
+        
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        String nv[];
+        for(int i = 0; i < N; i++){
+            nv = br.readLine().split(" ");
+            for(int j = 0; j < N; j++){
+                map[i][j] = Integer.parseInt(nv[j]);
+            }
+        }
+    }    
+}
