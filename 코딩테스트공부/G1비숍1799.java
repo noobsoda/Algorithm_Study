4b81a04ba63048e954c5087261736272fd3e69bc
noobsoda <20151565@vision.hoseo.edu>
2022-02-13 01:43:31 +0900

    2022_02_13 백트래킹 공부

--- 코딩테스트공부/G1비숍1799.java
+++ 코딩테스트공부/G1비숍1799.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class G1비숍1799 {
     static int N, max = 0;
     static int map[][];
+    static boolean colors[][];
+    static int res[] = new int[2];
     static int dx[] = {-1, -1, 1, 1};
     static int dy[] = {-1, 1, -1, 1};
 
@@ -26,43 +28,49 @@ public class G1비숍1799 {
         return true;
     }
 
-    public static void bishop(int x, int y, int cnt){
-        if(x >= N && y > N){
-            max = Math.max(max, cnt);
-            return;
+    static void solve(int index, boolean color, int cnt) {
+        for (int i = index+1; i < N*N; i++) {
+            int x = i / N;
+            int y = i % N;
+            
+            // 현재 탐색중인 색이 아니거나, 놓을 수 없거나, 대각선에 비숍이 존재하거나
+            if(colors[x][y] != color || map[x][y] == 0 || !check(x,y)) 
+                continue;
+            
+            // 백트래킹
+            map[x][y] = 2;
+            solve(i,color, cnt+1);
+            map[x][y] = 1;
         }
-        if(y > N){
-            bishop(x+1, y, cnt);
-            return;
-        }
-        if(map[x][y] == 1){
-            if(check(x, y)){
-                map[x][y] = 2;
-                bishop(x, y+1, cnt+1);
-                map[x][y] = 1;
-            }
+        if(color){
+            res[0] = Math.max(res[0], cnt);
         }
         else{
-            bishop(x, y+1, cnt);
+            res[1] = Math.max(res[1], cnt);
         }
-
     }
+
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         map = new int[N][N];
-
+        colors = new boolean[N][N];
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
+
+                colors[i][j] = (i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0);
             }
         }
-
-        bishop(0, 0, 0);
-        System.out.println(check(0, 0) ? "yes" : "No");
+        
+        solve(-1, true, 0);
+        solve(-1, false, 0);
+        
+        System.out.println(res[0] + res[1]);
     }
     
 }
