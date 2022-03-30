58f31e069ffabb0d809fe457bc1d69a50bb76156
noobsoda <20151565@vision.hoseo.edu>
2022-03-30 14:07:35 +0900

    2022_03_30 백준 정렬 공부

---
+++ 코딩테스트공부/S5수정렬하기3_10989.java
@@ -0,0 +1,29 @@
+package 코딩테스트공부;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class S5수정렬하기3_10989 {
+    static int N;
+    static int list[] = new int[10001]; 
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            list[n]++;
+        }
+        for(int n = 0; n < list.length; n++){
+            for(int i = 0; i < list[n]; i++){
+                bw.write(n + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+
+    }
+}
