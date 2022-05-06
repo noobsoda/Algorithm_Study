a6ae370805a40c58ed5019125e694d22a147eac3
noobsoda <20151565@vision.hoseo.edu>
2022-05-06 23:57:45 +0900

    2022_05_06 카카오 코테 공부

---
+++ 코딩테스트공부/L4호텔방배정64063.java
@@ -0,0 +1,27 @@
+package 코딩테스트공부;   
+import java.util.*;
+
+public class L4호텔방배정64063 {
+    static HashMap<Long, Long> hmap;
+    public long[] solution(long k, long[] room_number) {
+        long[] answer = new long[room_number.length];
+        hmap = new HashMap<>();
+        int cnt = 0;
+        for(long n : room_number){
+            answer[cnt++] = find(n);            
+        }
+        return answer;
+    }
+    public long find(long room){
+        if(!hmap.containsKey(room)){              
+            hmap.put(room, room+1);
+            return room;
+        }
+        long nextroom = hmap.get(room);
+        long emptyroom = find(nextroom);
+        hmap.put(room, emptyroom);
+        return emptyroom;
+         
+    }
+}
+//https://programmers.co.kr/learn/courses/30/lessons/64063
