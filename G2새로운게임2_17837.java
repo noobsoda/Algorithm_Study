a7c3471665751aef82dac2c1fd5dae144dcabe38
유제균 <sodamito3@gmail.com>
2022-07-18 17:05:59 +0900

    2022_07_18 테스트

--- G2새로운게임2_17837.java
+++ G2새로운게임2_17837.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class G2새로운게임2_17837 {
     static int N, K;
-    static int dx[] = {0, 0, 0, -1, 1};
-    static int dy[] = {0, 1, -1, 0, 0};
+    static int dx[] = {0, 0, -1, 1};
+    static int dy[] = {1, -1, 0, 0};
     static int map[][];
     static int visitmap[][];
     static List<Node> list;    
@@ -46,17 +46,18 @@ public class G2새로운게임2_17837 {
 
             int nx = node.x + dx[node.d];
             int ny = node.y + dy[node.d];
-            //위에 벽에 도달했을 시
-            if(nx < 1 || nx > N || ny < 1 || ny > N || map[nx][ny] == 2){
-                if(node.d == 1)    node.d = 2;
-                else if(node.d == 2)    node.d = 1;                
-                else if(node.d == 3)    node.d = 4;                
-                else if(node.d == 4)    node.d = 3;
+            //벽에 도달했을 시
+            if(nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] == 2){
+                if(node.d == 0 || node.d == 1)
+                    node.d = (node.d + 1) % 2;
+                else
+                    if(++node.d % 4 == 0)
+                        node.d = 2;
                 
                 nx = node.x + dx[node.d];
                 ny = node.y + dy[node.d];
 
-                if(nx < 1 || nx > N || ny < 1 || ny > N || map[nx][ny] == 2) continue;
+                if(nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] == 2) continue;
                 //반대쪽 벽도 못가거나 파란벽이면 컨티뉴
                 //반대쪽으로 바라보게 하고 이동시키기
             }            
@@ -91,28 +92,29 @@ public class G2새로운게임2_17837 {
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String nv[] = br.readLine().split(" ");
-        N = Integer.parseInt(nv[0]);
-        K = Integer.parseInt(nv[1]);
+        
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
         list = new LinkedList<>();
 
-        map = new int[N+1][N+1];
-        visitmap = new int[N+1][N+1];
-        for(int i = 1; i <= N; i++){
-            nv = br.readLine().split(" ");
-            for(int j = 1; j <= N; j++){
-                map[i][j] = Integer.parseInt(nv[j-1]);
+        map = new int[N][N];
+        visitmap = new int[N][N];
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         for(int i = 0; i < K; i++){
-            nv = br.readLine().split(" ");
-            int x = Integer.parseInt(nv[0]);
-            int y = Integer.parseInt(nv[1]);
-            int d = Integer.parseInt(nv[2]);
-            list.add(new Node(x, y, d, 1));
-            visitmap[x][y]++;
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            list.add(new Node(x-1, y-1, d-1, 1));
+            visitmap[x-1][y-1]++;
         }
 
         for(int i = 1; i <= 1001; i++){
