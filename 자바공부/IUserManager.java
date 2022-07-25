458c284f05325e3f3ab9fb16f9463a344f1c7dee
noobsoda <20151565@vision.hoseo.edu>
2022-07-25 15:26:29 +0900

    2022_07_25 인터페이스 공부

---
+++ 자바공부/IUserManager.java
@@ -0,0 +1,18 @@
+package 자바공부;
+
+// 인터페이스 정의
+public interface IUserManager {  
+	
+	void add(User user);
+	
+	User[] getList();
+	
+	User[] getUsers();
+	
+	VipUser[] getVipUsers();
+	
+	User[] searchByName(String name);
+	
+	double getAgeAvg();
+	
+}
