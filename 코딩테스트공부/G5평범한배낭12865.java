c8bab174350ca4638bab31356a3121c9376f32cb
soda <20151565@vision.hoseo.edu>
2023-01-24 03:01:14 +0900

    Docs : 2023_01_24 배낭구하기 DP공부

---
+++ 코딩테스트공부/G5평범한배낭12865.java
@@ -0,0 +1,45 @@
+package 코딩테스트공부;
+
+import java.io.*;
+import java.util.*;
+
+public class G5평범한배낭12865 {
+    static int N, K;
+    static int dp[][];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        dp = new int[N][K + 1];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int W = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            if (i == 0) {
+                for (int j = W; j <= K; j++) {
+                    dp[i][j] = V;
+                }
+            } else {
+                for (int j = 0; j <= K; j++) {
+                    if (j - W < 0) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W] + V);
+                    }
+                }
+            }
+
+        }
+        System.out.println(dp[N - 1][K]);
+
+    }
+
+}
+// https://www.acmicpc.net/problem/12865
+// 236ms
