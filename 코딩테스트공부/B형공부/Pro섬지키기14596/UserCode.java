76f0afd371d93d983fa65aaa768a7ab271623ed2
yujaegyun <sodamito3@gmail.com>
2023-03-02 17:56:34 +0900

    [Algo] B형 섬지키기 업데이트

--- 코딩테스트공부/B형공부/Pro섬지키기14596/UserCode.java
+++ 코딩테스트공부/B형공부/Pro섬지키기14596/UserCode.java
@@ -1,17 +1,55 @@
 package 코딩테스트공부.B형공부.Pro섬지키기14596;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class UserSolution {
     static int size;
-    static int islandMap[][];
-    static int tempMap[][];
+    static Map<String, Integer> hMap;
 
     public void init(int N, int mMap[][]) {
         size = N;
-        islandMap = new int[N][N];
-        tempMap = new int[2][N];
+        hMap = new HashMap<>();
 
-        for (int i = 0; i < N; i++) {
-            islandMap[i] = mMap[i].clone();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int m = 0;
+                StringBuilder sb = new StringBuilder();
+                int temp = -1;
+                // 가로
+                while (j + m < size) {
+                    if (m == 0)
+                        temp = mMap[i][j + m];
+                    else {
+                        sb.append(temp - mMap[i][j + m] + 5);
+                        temp = mMap[i][j + m];
+                        if (hMap.containsKey(sb.toString())) {
+                            hMap.put(sb.toString(), hMap.get(sb.toString()) + 1);
+                        } else {
+                            hMap.put(sb.toString(), 1);
+                        }
+                    }
+                    m++;
+                }
+                // 세로
+                m = 0;
+                sb = new StringBuilder();
+                while (i + m < size) {
+                    if (m == 0)
+                        temp = mMap[i + m][j];
+                    else {
+                        sb.append(temp - mMap[i + m][j] + 5);
+                        temp = mMap[i + m][j];
+                        if (hMap.containsKey(sb.toString())) {
+                            hMap.put(sb.toString(), hMap.get(sb.toString()) + 1);
+                        } else {
+                            hMap.put(sb.toString(), 1);
+                        }
+                    }
+                    m++;
+                }
+
+            }
 
         }
 
@@ -19,55 +57,37 @@ class UserSolution {
 
     public int numberOfCandidate(int M, int mStructure[]) {
         int res = 0;
-        for (int i = 0; i < size; i++) {
-            // 가로
-            for (int j = 0; j < size; j++) {
-                if (j + M - 1 >= size)
-                    break;
-                for (int k = 0; k < M; k++) {
-                    tempMap[0][j + k] = islandMap[i][j + k];
-                    tempMap[1][j + k] = islandMap[i][j + k];
-                }
-                for (int k = 0; k < M; k++) {
-                    tempMap[0][j + k] += mStructure[k];
-                    tempMap[1][j + k] += mStructure[M - 1 - k];
-                }
-                boolean flags[] = new boolean[2];
-                for (int k = 0; k < M - 1; k++) {
-                    for (int z = 0; z < 2; z++) {
-                        if (tempMap[z][j + k] != tempMap[z][j + k + 1])
-                            flags[z] = true;
-                    }
-                }
-                if (!flags[0] || !flags[1])
-                    res++;
+        int temp = -1;
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
 
+        if (M == 1) {
+            return size * size;
+        }
+        for (int i = 0; i < M; i++) {
+            if (i == 0)
+                temp = mStructure[i];
+            else {
+                sb1.append(temp - mStructure[i] + 5);
+                sb2.insert(0, temp - mStructure[i] + 5);
+                temp = mStructure[i];
             }
-            // 세로
-            if (M == 1)
-                continue;
-            for (int j = 0; j < size; j++) {
-                if (j + M - 1 >= size)
-                    break;
-                for (int k = 0; k < M; k++) {
-                    tempMap[0][j + k] = islandMap[j + k][i];
-                    tempMap[1][j + k] = islandMap[j + k][i];
-                }
-                for (int k = 0; k < M; k++) {
-                    tempMap[0][j + k] += mStructure[k];
-                    tempMap[1][j + k] += mStructure[M - 1 - k];
-                }
-                boolean flags[] = new boolean[2];
-                for (int k = 0; k < M - 1; k++) {
-                    for (int z = 0; z < 2; z++) {
-                        if (tempMap[z][j + k] != tempMap[z][j + k + 1])
-                            flags[z] = true;
-                    }
-                }
-                if (!flags[0] || !flags[1])
-                    res++;
+
+        }
+
+        if (sb1.toString().equals(sb2.toString())) {
+            if (hMap.containsKey(sb1.toString())) {
+                res += hMap.get(sb1.toString());
+            }
+        } else {
+            if (hMap.containsKey(sb1.toString())) {
+                res += hMap.get(sb1.toString());
+            }
+            if (hMap.containsKey(sb2.toString())) {
+                res += hMap.get(sb2.toString());
             }
         }
+
         return res;
     }
 
