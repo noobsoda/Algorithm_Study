cfa434f732103abfeb0ed67c002d51375b0282e3
soda <20151565@vision.hoseo.edu>
2023-03-29 02:22:37 +0900

    [Algo]B형 리스트 복사 설계 중

---
+++ 코딩테스트공부/B형공부/Pro리스트복사14613/UserSolution.java
@@ -0,0 +1,23 @@
+package 코딩테스트공부.B형공부.Pro리스트복사14613;
+
+class UserSolution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void init() {
+    }
+
+    public static void makeList(char mName[], int mLength, int mListValue[]) {
+    }
+
+    public static void copyList(char mDest[], char mSrc[], boolean mCopy) {
+    }
+
+    public static void updateElement(char mName[], int mIndex, int mValue) {
+    }
+
+    public static int element(char mName[], int mIndex) {
+        return 0;
+    }
+}
