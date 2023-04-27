b1f6df88d721de293a3a6aaa0fd9ad8329e9bd8c
soda <20151565@vision.hoseo.edu>
2023-04-28 02:22:35 +0900

    [Algo] 2023_04_26  B형 공부

---
+++ 코딩테스트공부/B형공부/Pro생산시스템13471/UserSolution.java
@@ -0,0 +1,15 @@
+package 코딩테스트공부.B형공부.Pro생산시스템13471;
+
+class UserSolution {
+    void init(int L, int M) {
+        return;
+    }
+
+    int request(int tStamp, int pId, int mLine, int eId, int mTime) {
+        return 0;
+    }
+
+    int status(int tStamp, int pId) {
+        return 0;
+    }
+}
