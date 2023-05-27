9a88b0f03eb2ed6c8011b784443c6beebec05ea9
soda <20151565@vision.hoseo.edu>
2023-05-27 15:54:48 +0900

    [Algo] 2023_05_27  DP 정사각형 Bottom-up 공부

--- 코딩테스트공부/백준/G4가장큰정사각형1915.java
+++ 코딩테스트공부/백준/G4가장큰정사각형1915.java
@@ -4,8 +4,8 @@ import java.io.*;
 import java.util.*;
 
 public class G4가장큰정사각형1915 {
-    static int N, M, dp;
-    static int arr[][];
+    static int N, M, maxSquareSize;
+    static int dp[][];
 
     public static void main(String[] args) throws IOException {
 
@@ -13,29 +13,29 @@ public class G4가장큰정사각형1915 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
+        dp = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             String s = st.nextToken();
             for (int j = 0; j < M; j++) {
-                arr[i][j] = s.charAt(j) - '0';
+                dp[i][j] = s.charAt(j) - '0';
             }
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 // 맨 윗줄이나 맨 왼쪽줄이거나 현재 값이 0이면 넘어가기
-                if (i == 0 || j == 0 || arr[i][j] == 0) {
-                    dp = Math.max(arr[i][j], dp);
+                if (i == 0 || j == 0 || dp[i][j] == 0) {
+                    maxSquareSize = Math.max(dp[i][j], maxSquareSize);
                     continue;
                 }
                 // 왼, 위, 왼위에서 가장 작은 값 +1
-                arr[i][j] = Math.min(Math.min(arr[i][j - 1], arr[i - 1][j]), arr[i - 1][j - 1]) + 1;
-                dp = Math.max(arr[i][j], dp);
+                dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                maxSquareSize = Math.max(dp[i][j], maxSquareSize);
             }
         }
-        System.out.println(dp * dp);
+        System.out.println(maxSquareSize * maxSquareSize);
 
     }
 
