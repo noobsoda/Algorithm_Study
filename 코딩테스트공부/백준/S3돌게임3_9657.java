9ed7f6fb1dbf9752cde913919c93d251d7598064
soda <20151565@vision.hoseo.edu>
2023-06-02 20:29:29 +0900

    [Algo] 2023_06_2 점화식 DP 공부

--- 코딩테스트공부/백준/S3돌게임3_9657.java
+++ 코딩테스트공부/백준/S3돌게임3_9657.java
@@ -3,10 +3,7 @@ package 코딩테스트공부.백준;
 import java.io.*;
 import java.util.*;
 
-/**
- * 단순 홀짝 문제
- */
-public class S3돌게임9659 {
+public class S3돌게임3_9657 {
     static long N;
 
     public static void main(String[] args) throws IOException {
@@ -14,9 +11,9 @@ public class S3돌게임9659 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Long.parseLong(st.nextToken());
 
-        System.out.println(N % 2 == 1 ? "SK" : "CY");
+        System.out.println(N % 7 == 0 || N % 7 == 2 ? "CY" : "SK");
 
     }
 
 }
-// https://www.acmicpc.net/problem/9659
+// https://www.acmicpc.net/problem/9657
