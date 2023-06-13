e007c3975f1d5fc23f6bdf07163460d2f08801c3
soda <20151565@vision.hoseo.edu>
2023-06-13 23:11:01 +0900

    [Algo] 2023_06_13 세그트리 공부

--- 코딩테스트공부/백준/G1커피숍2_1275.java
+++ 코딩테스트공부/백준/G1커피숍2_1275.java
@@ -35,12 +35,65 @@ public class G1커피숍2_1275 {
     }
 
     static class SegmentTree {
-        public SegmentTree(int arr[]) {
+        int[] tree;
+        int[] arr;
 
+        public SegmentTree(int[] arr) {
+            int n = arr.length;
+            tree = new int[4 * n];
+            this.arr = arr;
+            buildTree(1, 0, n - 1);
+        }
+
+        private void buildTree(int node, int start, int end) {
+            if (start == end) {
+                tree[node] = arr[start];
+                return;
+            }
+
+            int mid = (start + end) / 2;
+            buildTree(node * 2, start, mid);
+            buildTree(node * 2 + 1, mid + 1, end);
+            tree[node] = tree[node * 2] + tree[node * 2 + 1];
+        }
+
+        public void update(int idx, int val) {
+            updateTree(1, 0, arr.length - 1, idx, val);
+        }
+
+        private void updateTree(int node, int start, int end, int idx, int val) {
+            if (start == end) {
+                arr[idx] = val;
+                tree[node] = val;
+                return;
+            }
+
+            int mid = (start + end) / 2;
+            if (idx <= mid) {
+                updateTree(node * 2, start, mid, idx, val);
+            } else {
+                updateTree(node * 2 + 1, mid + 1, end, idx, val);
+            }
+            tree[node] = tree[node * 2] + tree[node * 2 + 1];
         }
 
         public int query(int left, int right) {
-            return 0;
+            return queryTree(1, 0, arr.length - 1, left, right);
+        }
+
+        private int queryTree(int node, int start, int end, int left, int right) {
+            if (right < start || end < left) {
+                return 0;
+            }
+
+            if (left <= start && end <= right) {
+                return tree[node];
+            }
+
+            int mid = (start + end) / 2;
+            int sumLeft = queryTree(node * 2, start, mid, left, right);
+            int sumRight = queryTree(node * 2 + 1, mid + 1, end, left, right);
+            return sumLeft + sumRight;
         }
     }
 }
