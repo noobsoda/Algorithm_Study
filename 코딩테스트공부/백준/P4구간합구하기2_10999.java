1255d7613d85f16dfac7a8617b117295a3c57f66
soda <20151565@vision.hoseo.edu>
2023-06-27 19:03:52 +0900

    [Algo] 2023_06_27 Lazy Segment Tree 공부

--- 코딩테스트공부/백준/P4구간합구하기2_10999.java
+++ 코딩테스트공부/백준/P4구간합구하기2_10999.java
@@ -34,5 +34,67 @@ public class P4구간합구하기2_10999 {
         }
 
     }
+    static class SegmentTree {
+        long[] tree;
+        int[] arr;
+
+        public SegmentTree(int[] arr) {
+            int n = arr.length;
+            tree = new long[4 * n];
+            this.arr = arr;
+            buildTree(1, 0, n - 1);
+        }
+
+        private void buildTree(int node, int start, int end) {
+            if (start == end) {
+                tree[node] = arr[start];
+                return;
+            }
+
+            int mid = (start + end) / 2;
+            buildTree(node * 2, start, mid);
+            buildTree(node * 2 + 1, mid + 1, end);
+            tree[node] = tree[node * 2] + tree[node * 2 + 1];
+        }
+
+        public void update(int idx, int val) {
+            updateTree(1, 0, arr.length - 1, idx, val);
+        }
+
+        private void updateTree(int node, int start, int end, int idx, int val) {
+            if (start == end) {
+                arr[idx] = val;
+                tree[node] = val;
+                return;
+            }
+
+            int mid = (start + end) / 2;
+            if (idx <= mid) {
+                updateTree(node * 2, start, mid, idx, val);
+            } else {
+                updateTree(node * 2 + 1, mid + 1, end, idx, val);
+            }
+            tree[node] = tree[node * 2] + tree[node * 2 + 1];
+        }
+
+        public long query(int left, int right) {
+            return queryTree(1, 0, arr.length - 1, left, right);
+        }
+
+        private long queryTree(int node, int start, int end, int left, int right) {
+            if (right < start || end < left) {
+                return 0;
+            }
+
+            if (left <= start && end <= right) {
+                return tree[node];
+            }
+
+            int mid = (start + end) / 2;
+            long sumLeft = queryTree(node * 2, start, mid, left, right);
+            long sumRight = queryTree(node * 2 + 1, mid + 1, end, left, right);
+            return sumLeft + sumRight;
+        }
+    }
 }
 // https://www.acmicpc.net/problem/10999
