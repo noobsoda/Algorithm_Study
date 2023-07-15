adbe3b125e25d32537fc12b1797312107cbe9b22
soda <20151565@vision.hoseo.edu>
2023-07-16 01:04:52 +0900

    [Algo] 2023_07_16 Tree 문제 풀이

--- 코딩테스트공부/백준/G5노드사이의거리1240.java
+++ 코딩테스트공부/백준/G5노드사이의거리1240.java
@@ -12,6 +12,7 @@ public class G5노드사이의거리1240 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
@@ -23,6 +24,45 @@ public class G5노드사이의거리1240 {
             list[i] = new ArrayList<>();
 
         }
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            list[s].add(new Node(e, w));
+            list[e].add(new Node(s, w));
+
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            bw.write(String.format(bfs(s, e) + "\n"));
+        }
+        bw.flush();
+    }
+
+    private static int bfs(int s, int e) {
+        Queue<Node> q = new ArrayDeque<>();
+        boolean visited[] = new boolean[N + 1];
+        q.add(new Node(s, 0));
+        visited[s] = true;
+
+        while (!q.isEmpty()) {
+            Node now = q.poll();
+            for (Node next : list[now.e]) {
+                if (visited[next.e])
+                    continue;
+                if (next.e == e) {
+                    return now.w + next.w;
+                }
+                visited[next.e] = true;
+                q.add(new Node(next.e, now.w + next.w));
+            }
+        }
+
+        return 0;
+
     }
 
     static class Node {
@@ -34,3 +74,4 @@ public class G5노드사이의거리1240 {
         }
     }
 }
+// https://www.acmicpc.net/problem/1240
